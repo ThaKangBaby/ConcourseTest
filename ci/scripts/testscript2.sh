ee6d995f654b2f36d0ed8375e3399deddea7cb47
Sonny Singh <thakangbaby@gmail.com>
2016-11-15 15:07:53 +0100

    Added ssh remote for testscript2

--- ci/scripts/testscript2.sh
+++ ci/scripts/testscript2.sh
@@ -27,11 +27,10 @@ git checkout master
 git config user.email "thakangbaby@gmail.com"
 git config user.name "thakangbaby"
 
-#git remote set-url origin https://thakangbaby:17smsj2115@github.com/thakangbaby/ConcourseTest.git
-#thakangbaby@gmail.com:ThaKangBaby/ConcourseTest.git
+git remote set-url origin thakangbaby@gmail.com:ThaKangBaby/ConcourseTest.git
 
 git merge ready -m "Trying to merge with script"
 
-git push #--repo https://thakangbaby:17smsj2115@github.com/thakangbaby/ConcourseTest.git
+git push
 
 git status
