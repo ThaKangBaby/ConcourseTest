b6ae80e2c6dca793e4d616389bad4a877d7bf4e4
Sonny Singh <thakangbaby@gmail.com>
2016-12-21 11:01:11 +0100

    Corrected the link to the git repo

--- ci/scripts/branch-quest.sh
+++ ci/scripts/branch-quest.sh
@@ -3,8 +3,9 @@
 set -e # fail fast
 set -x # print commands
 
-git clone repository ConcourseTest
-cd integration
+git clone https://github.com/ThaKangBaby/ConcourseTest.git
+cd ConcourseTest
+ll
 
 
 for remote in `git branch -r`
