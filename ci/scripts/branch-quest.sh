0cb966f8eea4ad8d3aabcb260e1c513f942a3086
Sonny Singh <thakangbaby@gmail.com>
2016-12-21 10:57:01 +0100

    Changed a git clone reference to be correct

--- ci/scripts/branch-quest.sh
+++ ci/scripts/branch-quest.sh
@@ -3,16 +3,14 @@
 set -e # fail fast
 set -x # print commands
 
-git clone repository integration
+git clone repository ConcourseTest
 cd integration
 
 
 for remote in `git branch -r`
-do 
+do
 if [[ $remote == *"origin/ready/"* ]]
 then
   echo $remote>ready_branch;
 fi
 done
-
-
