c324dfd2949744f182e358efae849d46ca0c1867
Sonny Singh <thakangbaby@gmail.com>
2016-12-21 11:20:42 +0100

    Changed the job-integration.yml for testing purposes

--- ci/scripts/integration.sh
+++ ci/scripts/integration.sh
@@ -3,10 +3,15 @@
 set -e # fail fast
 set -x # print commands
 
-git clone repository integration
+ls
 cd integration
 ls
-echo "hello"
-git checkout `cat ready_branch`
-echo "success!"
 git status
+
+# git clone repository integration
+# cd integration
+# ls
+# echo "hello"
+# git checkout `cat ready_branch`
+# echo "success!"
+# git status
