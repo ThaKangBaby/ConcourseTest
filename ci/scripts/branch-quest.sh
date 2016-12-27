76d4dcc56241098fb29ea7189deaf3560dfdf9c6
Sonny Singh <thakangbaby@gmail.com>
2016-12-27 11:49:05 +0100

    Changed script in branch-quest.sh so it also outputs the file ready_branch

--- ci/scripts/branch-quest.sh
+++ ci/scripts/branch-quest.sh
@@ -11,14 +11,11 @@ cd ConcourseTest
 
 git branch -r
 
-if git branch -r | grep 'origin/ready/'* &> /dev/null; then
-   echo $?
+for remote in `git branch -r`
+do
+if [[ $remote == *"origin/ready/"* ]]
+then
+  echo $remote>ready_branch;
+  cat ready_branch;
 fi
-
-# for remote in `git branch -r`
-# do
-# if [[ $remote == *"origin/ready/"* ]]
-# then
-#   echo $remote>ready_branch;
-# fi
-# done
+done
