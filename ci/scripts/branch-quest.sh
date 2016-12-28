f689ccf7333aaca716cf0fea4de4296a0da868f9
Sonny Singh <thakangbaby@gmail.com>
2016-12-28 08:54:49 +0100

    Minor change in script branch-quest.sh

--- ci/scripts/branch-quest.sh
+++ ci/scripts/branch-quest.sh
@@ -16,14 +16,19 @@ do
 if [[ "$remote" == *"origin/ready/"* ]]
 then
   echo $remote>ready_branch;
-  cat ready_branch;
+  #cat ready_branch;
 fi
 done
 
+git config --global user.email "thakangbaby@gmail.com"
+git config --global user.name "Sonny Singh"
 
 git checkout master
 
-git merge --ff-only $(cat ready_branch)
+git merge --squash $(cat ready_branch)
+git commit -m $(cat ready_branch) --author ConcourseCI
+
+
 
 #git push $TARGET_REPO HEAD:$TARGET_BRANCH
 #git push $TARGET_REPO :$TRAVIS_BRANCH
