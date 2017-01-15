01208a4c3d0d827a2ecca843486bf1451c8a3354
Sonny Singh <thakangbaby@gmail.com>
2017-01-15 18:44:12 +0100

    Updated script pseudo with changes from Lars

--- ci/scripts/buildtest.sh
+++ ci/scripts/buildtest.sh
@@ -5,6 +5,34 @@ set -x # print commands
 
 ls -la
 
+#git clone https://github.com/ThaKangBaby/ConcourseTest.git integration
+
+#Checking for readybranch
+cd CheckingBranches
+readybranch=$(readybranch=$(git branch -r --list "origin/ready/*" | tail -1 | sed "s/^[ \t]*//"))
+
+
+git checkout $readybranch
+
+git pull --rebase master
+
+git checkout master
+
+git merge --squash $readybranch
+
+#TEST
+TEST
+
+#On test success
+Push master to origin
+
+#Do this no matter outcome
+Delete the ready branch remotely
+git fetch --prune
+
+#if [ $readybranch != "" ]
+
+#Testing for Jekyll buildtest
 jekyll --version
 
 jekyll build --source Praqma.com --destination integration
