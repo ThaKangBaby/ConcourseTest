eae6bbc5ad1dc4ae7376f71070ece27d9cb02c89
alpap <al.pap@live.com>
2017-01-19 10:45:44 +0100

    deleted some comments

--- ci/scripts/checkreadybranch.sh
+++ ci/scripts/checkreadybranch.sh
@@ -13,11 +13,11 @@ pwd
 #git clone Praqma.com integration
 mkdir -p ~/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-chmod 0666 ~/.ssh/config
+chmod 777 ~/.ssh/config
 
 #ok
 echo -e "{{$private_key}}" > ~/.ssh/key.pem
-chmod 0666 ~/.ssh/key.pem
+chmod 777 ~/.ssh/key.pem
 ssh-agent $(ssh-add key.pem; git clone git@github.com:ThaKangBaby/praqma.com.git)
 
 
